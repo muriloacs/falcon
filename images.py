00594062f6aee20ba85b2b2ab11f4341649555a2
Murilo Amaral Santiago <muriloacs@yahoo.com.br>
2015-08-26 00:56:03 -0300

    simplest example

--- images.py
+++ images.py
@@ -1,17 +1,12 @@
 import falcon
 
 
-def validate_browser(req, resp, resource, params):
-    if 'Chrome' not in req.user_agent:
-        raise falcon.HTTPBadRequest('Bad request', 'Only chrome is allowed')
-
 class Resource(object):
 
     def on_get(self, req, resp):
         resp.body = '{"message": "Getting Images!"}'
         resp.status = falcon.HTTP_200   
 
-    @falcon.before(validate_browser)
     def on_post(self, req, resp):
         resp.body = '{"message": "Posting Images!"}'
         resp.status = falcon.HTTP_201
